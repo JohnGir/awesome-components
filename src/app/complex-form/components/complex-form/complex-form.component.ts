0749d0d60748da4dea079df7e12028c003932476
ZORO <giral.zoro@mqashgroup.com>
2023-05-22 12:58:41 +0000

    start joinning template

--- src/app/complex-form/components/complex-form/complex-form.component.ts
+++ src/app/complex-form/components/complex-form/complex-form.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-complex-form',
@@ -7,17 +12,61 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./complex-form.component.scss'],
 })
 export class ComplexFormComponent implements OnInit {
-  mainForm!: FormGroup;
+  //
+  mainForm: FormGroup;
+  personalInfoForm: FormGroup;
+  contactPreferenceCtrl: FormControl;
+  emailForm: FormGroup;
+  emailCtrl: FormControl;
+  confirmEmailCtrl: FormControl;
+  phoneCtrl: FormControl;
+  passwordCtrl: FormControl;
+  confirmPasswordCtrl: FormControl;
+  loginInfoForm: FormGroup;
 
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
     this.initMainForm();
+    this.initFormControls();
   }
 
-  initMainForm(): void {
-    this.mainForm = this.formBuilder.group({});
+  private initMainForm(): void {
+    this.mainForm = this.formBuilder.group({
+      personalInfo: this.personalInfoForm,
+      contactPreference: this.contactPreferenceCtrl,
+      email: this.emailForm,
+      phone: this.phoneCtrl,
+      loginInfo: this.loginInfoForm,
+    });
   }
 
-  onSubmitForm() {}
+  onSubmitForm() {
+    console.log(this.mainForm.value);
+  }
+
+  private initFormControls(): void {
+    this.personalInfoForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+    });
+    this.contactPreferenceCtrl = this.formBuilder.control('email');
+    this.emailCtrl = this.formBuilder.control('');
+    this.confirmEmailCtrl = this.formBuilder.control('');
+    this.emailForm = this.formBuilder.group({
+      email: this.emailCtrl,
+      confirm: this.confirmEmailCtrl,
+    });
+    this.phoneCtrl = this.formBuilder.control('');
+    this.passwordCtrl = this.formBuilder.control('', Validators.required);
+    this.confirmPasswordCtrl = this.formBuilder.control(
+      '',
+      Validators.required
+    );
+    this.loginInfoForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: this.passwordCtrl,
+      confirmPassword: this.confirmPasswordCtrl,
+    });
+  }
 }
